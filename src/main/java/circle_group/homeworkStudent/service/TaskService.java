e151c13a8f33ecc2760132fcd6a992e01ad1059a
Abdulkhayev Atkhambek <abdulkhayevatkham.0203@gmail.com>
2022-08-24 21:49:33 +0500

    Service task

--- src/main/java/circle_group/homeworkStudent/service/TaskService.java
+++ src/main/java/circle_group/homeworkStudent/service/TaskService.java
@@ -1,4 +1,24 @@
 package circle_group.homeworkStudent.service;
 
+import circle_group.homeworkStudent.dto.ResponseDto;
+import circle_group.homeworkStudent.dto.TaskDto;
+import circle_group.homeworkStudent.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.util.MultiValueMap;
+
 public interface TaskService {
+
+    ResponseDto<String> addTask(TaskDto taskDto);
+
+    ResponseDto<TaskDto> getById(Integer id);
+
+    ResponseDto<Page<TaskDto>> getAll();
+
+    ResponseDto<TaskDto> updateTask(TaskDto taskDto);
+
+    ResponseDto<Task> deleteTask(Integer id);
+
+    ResponseDto<Page<TaskDto>> byParam(MultiValueMap<String,String> params);
+
+
 }
--- src/main/java/circle_group/homeworkStudent/service/impl/TaskImpl.java
+++ src/main/java/circle_group/homeworkStudent/service/impl/TaskImpl.java
@@ -1,10 +1,48 @@
 package circle_group.homeworkStudent.service.impl;
 
+import circle_group.homeworkStudent.dto.ResponseDto;
+import circle_group.homeworkStudent.dto.TaskDto;
+import circle_group.homeworkStudent.entity.Task;
+import circle_group.homeworkStudent.repository.TaskRepository;
 import circle_group.homeworkStudent.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 
 @Service
 @RequiredArgsConstructor
 public class TaskImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Override
+    public ResponseDto<String> addTask(TaskDto taskDto) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<TaskDto> getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<Page<TaskDto>> getAll() {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<TaskDto> updateTask(TaskDto taskDto) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<Task> deleteTask(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<Page<TaskDto>> byParam(MultiValueMap<String, String> params) {
+        return null;
+    }
 }
